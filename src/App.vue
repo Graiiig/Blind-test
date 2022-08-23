218de79d9d736a557bd85c8935222d10acbd47d1
Grégory Verguin <gregory.verguin@additi.fr>
2022-08-23 13:55:17 +0200

    :sparkles: v1.0.2 : Ajout récup data depuis firebase

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,21 @@
   <router-view></router-view>
 </template>
 <script>
+import {db, onValue, ref} from "@/assets/js/firebase";
+
 export default {
   created : function(){
-    document.querySelector('#app').style.backgroundImage = "url('https://images.unsplash.com/photo-1466428996289-fb355538da1b?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzNDg3OTJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTg1OTQ2OTU&ixlib=rb-1.2.1&q=80')";
+
+    // Pour l'instant, on met une image en dur en background
+    document.querySelector('#app').style.backgroundImage = "url('https://images.unsplash.com/photo-1501612780327-45045538702b?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzNDg3OTJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTg1OTQ2Mzc&ixlib=rb-1.2.1&q=80')";
+
+    // Récupération des datas depuis Firebase, dernier qui a buzzé + status musique
+    let dbFb = ref(db, '/');
+    onValue(dbFb, (data) => {
+      let dataFromDb = data.val();
+      this.$store.commit('setIsMusicPlaying', dataFromDb.appSettings.isMusicPlaying)
+      this.$store.commit('setClicker', dataFromDb.clicker.nom)
+    });
   }
 }
 </script>
