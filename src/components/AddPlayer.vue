3d75e66f7a48f26a34bdedab21bc59354bd66901
Grégory Verguin <gregory.verguin@additi.fr>
2022-08-25 09:21:57 +0200

    :sparkles: v1.0.3 : refacto

--- src/components/AddPlayer.vue
+++ src/components/AddPlayer.vue
@@ -26,13 +26,13 @@ export default {
       }).key;
 
       // On cache le menu d'ajout d'un nouvel utilisateur
-      this.$store.state.showMenuAddPlayer = false;
+      this.$store.commit('setShowMenuAddPlayer', false)
     },
   }
 }
 </script>
 <template>
-  <div class="card" :style="this.$store.state.showMenuAddPlayer ? 'visibility: visible;' : 'visibility: hidden;'">
+  <div class="card" :style="this.$store.getters.getShowMenuAddPlayer ? 'visibility: visible;' : 'visibility: hidden;'">
     <div class="addUser flex space-around">
       <input id="username" placeholder="Pseudo">
       <span id="addUser" class="button" @click="addUser">Ajouter un utilisateur</span>
