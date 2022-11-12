e257efc20ec55ad6b7f60cc568824419d5f7bf74
Grégory VERGUIN <gregory.verguin@hotmail.fr>
2022-11-12 16:47:42 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : process.env.VITE_PUBLIC_PATH,
+  base : '/',
   plugins: [vue()],
   resolve: {
     alias: {
