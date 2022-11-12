ce4f0436e82443e2cb53016c2b38d37613ebb954
CI <CI@example.com>
2022-11-12 14:20:56 +0000

    deploy

---
+++ main.js
@@ -0,0 +1,29 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Buzzer from './components/Buzzer.vue'
+import Home from './components/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import './assets/styles.css'
+import store from './store.js'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path : '/', name: 'Home', component: Home, props: true},
+        {path : '/buzzer/:username', name: 'Buzzer', component: Buzzer},
+    ]
+})
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlay, faPause, faPlusCircle, faMusic, faTrashCan, faHome, faCompactDisc, faMicrophoneLines, faCircleXmark, faForwardStep, faBackwardStep,
+    faQrcode} from "@fortawesome/free-solid-svg-icons";
+// Ajout des icones dont on a besoin à la librairie
+library.add(faPlay, faPause, faPlusCircle, faMusic, faTrashCan, faHome, faCompactDisc, faMicrophoneLines, faCircleXmark, faForwardStep, faBackwardStep, faQrcode);
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .use(router)
+    .use(store)
+    .mount('#app')
