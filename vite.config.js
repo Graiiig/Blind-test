4b7830e36093f1e957c817d39813d86e3d6c0a75
Grégory VERGUIN <gregory.verguin@hotmail.fr>
2022-11-12 14:35:36 +0100

    Ajout publicpath et env.production

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : process.env.NODE_ENV === 'production' ? '/Blind-test/' : '/',
+  base : process.env.VITE_PUBLIC_PATH,
   plugins: [vue()],
   resolve: {
     alias: {
