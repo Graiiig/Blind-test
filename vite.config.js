470340b47160d6044e0bc2838a2dbed55be38375
Grégory VERGUIN <gregory.verguin@hotmail.fr>
2022-11-12 15:20:32 +0100

    test deplacement main.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : '/',
+  base : process.env.VITE_PUBLIC_PATH,
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: '0.0.0.0'
   }
 })
